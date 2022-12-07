e926706e740c743231705457e5b900b1ac305ef1
MAbdurahman <mdbdrrhm@gmail.com>
2022-12-07 06:37:45 -0600

    Add preloader to project

---
+++ public/js/scripts.js
@@ -0,0 +1,7 @@
+/* =========================================
+            Preloader
+============================================ */
+$(window).on('load', function () {
+	// makes sure that whole site is loaded
+	$('#preloader--gif, #preloader').fadeOut(2550, function () {});
+});
