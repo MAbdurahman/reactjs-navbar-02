94dabb0eb9dcf524ca5c30aa160bfb5f594abe27
MAbdurahman <mdbdrrhm@gmail.com>
2023-10-02 04:07:53 -0500

    Update components/Navbar.jsx and components/Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Contact() {
-	return (
-		<div id='contact' className='page scrollspy'>
-			<h1 className='heading-text'>Contact Me</h1>
-		</div>
-	);
+export default function Contact () {
+  return (
+    <div id="contact" className="page scrollspy">
+      <h1 className="heading-text">Contact Me</h1>
+    </div>
+  );
 }
